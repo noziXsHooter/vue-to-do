16bc7d4280c64cce6e6ea61a322c4e59a458d3e7
noziX <94942615+noziXsHooter@users.noreply.github.com>
2022-09-19 21:00:37 -0300

    1

--- api/todos.js
+++ api/todos.js
@@ -1,18 +1,8 @@
 export default function handler(req, res) {
 
       var pg = require('pg');
-      //or native libpq bindings
-      //var pg = require('pg').native
-
       var conString = "postgres://tmdfyodj:m5RipS-70x_0rCUB_Nndh_dJ7sAPP9fS@kesavan.db.elephantsql.com/tmdfyodj" //Can be found in the Details page
       var client = new pg.Client(conString);
-      var getTodos = {
-        "todos": [
-          {
-            "title": "Toma café",
-            "completed": true,
-            "id": 1
-          },]}
 
       if(req.method === 'GET'){
 
@@ -24,7 +14,6 @@ export default function handler(req, res) {
             if(err) {
               return console.error('error running query', err);
             }
-             // res.status(200).send(JSON. stringify({todos:result.rows}))
             res.status(200).send(result.rows)
             client.end();
           });
